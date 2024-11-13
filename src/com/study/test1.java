466bbba034548066b3219eecb9cb6e8c83064471
31530 <3153094221@qq.com>
2024-11-13 18:46:38 +0800

    Changes

---
+++ src/com/study/test1.java
@@ -0,0 +1,8 @@
+package com.study;
+
+public class test1 {
+    public static void main(String[] args){
+        System.out.println("hello,world");
+
+    }
+}
---
+++ src/com/study/testchangechar.java
@@ -0,0 +1,12 @@
+package com.study;
+/**
+*  @author:齐韬
+*
+* */
+public class testchangechar {
+    public static void main(String[] args){
+        int a = 10;
+        System.out.println("北京\t上海\t");
+        System.out.println(a);
+    }
+}//转义字符的使用需求
