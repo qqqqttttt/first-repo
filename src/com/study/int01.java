6967c4a307c856ba8ebded10bc096293a8d416c7
qqqqttttt <3153094221@qq.com>
2024-11-14 18:21:55 +0800

    Changes

--- src/com/study/int01.java
+++ src/com/study/int01.java
@@ -8,5 +8,17 @@ public class int01 {
         char s = 's';
         double s1 = 1.223243423;
         System.out.println(s1);
+        double num1 = 2.7;
+        double num2 = 8.1/3;
+        System.out.println(num1);//当我们对运算结果为小数的判断为标准时，要小心，应当是以两个数的差值的绝对值的精度值判断。
+        System.out.println(num2);
+
+       if(num1 == num2){
+           System.out.println("相等");
+       }
+       if(Math.abs(num1 - num2)<0.0000001){
+           System.out.println("相等");
+       }
+       System.out.println(Math.abs(num1 - num2));
     }
 }
