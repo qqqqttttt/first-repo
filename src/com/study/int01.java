eca41dc1058afb1fbc61f978f362f81ae51f2d70
qqqqttttt <3153094221@qq.com>
2024-11-14 10:59:06 +0800

    Changes

---
+++ src/com/study/int01.java
@@ -0,0 +1,12 @@
+package com.study;
+
+public class int01 {
+    public static void main(String[] args){
+
+        int n1 = 4;//4个字节
+        long n2 = 2L;//8个字节
+        char s = 's';
+        double s1 = 1.223243423;
+        System.out.println(s1);
+    }
+}
